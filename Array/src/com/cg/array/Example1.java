f56d38b61720288cc75505af96bc40a8f26b1681
Admin <Admin@DESKTOP-G101JLL>
2022-08-01 22:22:13 +0530

    Commit

---
+++ Array/src/com/cg/array/Example1.java
@@ -0,0 +1,11 @@
+package com.cg.array;
+
+public class Example1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a[]= {1,6,9,7};
+		System.out.println(a[2]);
+	}
+
+}
