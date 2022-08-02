8c487cbfcb27b6ba6b576a25535f8081efe3cba1
Admin <Admin@DESKTOP-G101JLL>
2022-08-02 16:34:39 +0530

    Commit

---
+++ Assignment11/src/com/cg/assignment/PalindromeString.java
@@ -0,0 +1,22 @@
+package com.cg.assignment;
+
+import java.util.Scanner;
+
+public class PalindromeString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String original, reverse = ""; // Objects of String class  
+	      Scanner in = new Scanner(System.in);   
+	      System.out.println("Enter a string to check if it is a palindrome");  
+	      original = in.nextLine();   
+	      int length = original.length();   
+	      for ( int i = length - 1; i >= 0; i-- )  
+	         reverse = reverse + original.charAt(i);  
+	      if (original.equals(reverse))  
+	         System.out.println("Entered string is a palindrome.");  
+	      else  
+	         System.out.println("Entered string isn't a palindrome.");   
+	}
+
+}
---
+++ Assignment11/src/com/cg/assignment/Welcome.java
@@ -0,0 +1,10 @@
+package com.cg.assignment;
+
+public class Welcome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome TNS Foundation");
+	}
+
+}
